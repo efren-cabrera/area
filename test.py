74115a00c9b342e7f32dac23691184ab223f5352
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-07-25 11:37:41 -0700

    Delete old test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from area import area, area_from_lat_lon_list
+from area import area
 
 
 f = open('illinois.json')
@@ -29,37 +29,8 @@ world_area = 511207893395811.06
 
 class AreaTestCase(unittest.TestCase):
 
-    def test_area_illinois_with_string(self):
-        """ Computer the area of illinois with string input """
-
-        # Go the top of the file
-        f.seek(0)
-        illinois = f.read()
-        self.assertEqual(round(area(illinois), 2), round(illinois_area, 2))
-
-    def test_area_illinois(self):
-        """ Compute the area of illinois """
-        self.assertEqual(round(area(illinois), 2), round(illinois_area, 2))
-
-    def test_area_world(self):
-        """ Compute the area of the whole world """
-        self.assertEqual(area(world), world_area)
-
-    def test_point_area(self):
-        """ Compute the area of a point """
-        self.assertEqual(area({'type': 'Point', 'coordinates': [0, 0]}), 0)
-
-    def test_liststring_area(self):
-        """ Compute the area of a line string """
-        self.assertEqual(area({'type': 'LineString', 'coordinates': [[0, 0], [1, 1]]}), 0)
-
-    def test_geometry_collection_area(self):
-        """ Compute the area of a geometry collection """
-        total = illinois_area + world_area
-        self.assertEqual(area({'type': 'GeometryCollection', 'geometries': [world, illinois]}), total)
-
     def test_lat_lon_list_area(self):
-        self.assertEqual(area_from_lat_lon_list(latitude_world, longitude_world), world_area)
+        self.assertAlmostEqual(area(latitude_world, longitude_world), world_area, places=0)
 
 
 if __name__ == '__main__':
