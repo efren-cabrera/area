1aa34c4052fb8af42b6327eecb11ae8f85d8e57b
Efren Cabrera <efren507.507@gmail.com>
2019-07-25 12:02:46 -0700

    Add test to check Illinois area

--- test.py
+++ test.py
@@ -23,14 +23,23 @@ world = {
 latitude_world = [-90, 90, 90, -90, -90]
 longitude_world = [-180, -180, 180, 180, -180]
 
-illinois_area = 145978332359.36746
+ILLINOIS_AREA = 145978332359.36746
 world_area = 511207893395811.06
 
 
 class AreaTestCase(unittest.TestCase):
 
-    def test_lat_lon_list_area(self):
+    def test_world_area(self):
         self.assertAlmostEqual(area(latitude_world, longitude_world), world_area, places=0)
+
+    def test_illinois_area(self):        
+        illinois_area = area(*self.get_lat_lon(illinois["coordinates"][0][0]))
+        self.assertAlmostEqual(illinois_area, ILLINOIS_AREA, places=3)
+
+    def get_lat_lon(self, coordinate_pairs_list):
+        latitude = [coordinate_pair[1] for coordinate_pair in coordinate_pairs_list]
+        longitude = [coordinate_pair[0] for coordinate_pair in coordinate_pairs_list]
+        return latitude, longitude
 
 
 if __name__ == '__main__':
